d971e2eb13a6ee12cf0eea9110e54e4e996e4363
Grindre <105343090+Grindre@users.noreply.github.com>
2023-01-12 22:04:17 +0800

    Fix copyright header

--- jest.config.ts
+++ jest.config.ts
@@ -13,4 +13,7 @@ export default {
 	// The directory where Jest should store its cached dependency information
 	// cacheDirectory: "/private/var/folders/35/4k90_5c16cj9kxg9knbhj9g40000gn/T/jest_dx",
 
-	// Automatically clear mock calls, instances, contexts and results
+	// Automatically clear mock calls, instances, contexts and results before every test
+	// clearMocks: false,
+
+	// Indicates whether the cove
