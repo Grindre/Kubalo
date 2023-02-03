2e040d9c20c87541ccb802769594420e0f82b180
Grindre <105343090+Grindre@users.noreply.github.com>
2023-02-03 09:35:05 +0800

    Update AI.py

--- jest.config.ts
+++ jest.config.ts
@@ -19,4 +19,4 @@ export default {
 	// Indicates whether the coverage information should be collected while executing the test
 	collectCoverage : true,
 
-	// An array of glob patterns indica
+	// An array of glob patterns indicating a set of files for which coverage information should be collect
