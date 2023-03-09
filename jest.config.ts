41267f03c8dbd31d15ed644e5dfc833f5eb38a48
Grindre <105343090+Grindre@users.noreply.github.com>
2023-03-10 07:00:07 +0800

    gitkeep

--- jest.config.ts
+++ jest.config.ts
@@ -58,4 +58,7 @@ export default {
 	// Force coverage collection from ignored files using an array of glob patterns
 	// forceCoverageMatch: [],
 
-	// A path to a module which exports an async function that is triggered once before a
+	// A path to a module which exports an async function that is triggered once before all test suites
+	// globalSetup: undefined,
+
+	// A path to a 
