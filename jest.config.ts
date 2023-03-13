c2e211abd12dbea99e180863edf242c1badc276d
Grindre <105343090+Grindre@users.noreply.github.com>
2023-03-13 19:47:13 +0800

    Fixed a typo in a comment

--- jest.config.ts
+++ jest.config.ts
@@ -64,4 +64,7 @@ export default {
 	// A path to a module which exports an async function that is triggered once after all test suites
 	// globalTeardown: undefined,
 
-	// A set of global variables that need to be available in all test e
+	// A set of global variables that need to be available in all test environments
+	// globals: {},
+
+	// The maximum amount of workers us
