7823cde8324721f5b6576c26d2ae8a3e3f5200d7
Grindre <105343090+Grindre@users.noreply.github.com>
2023-04-28 06:25:09 +0800

    Readme typo

--- jest.config.ts
+++ jest.config.ts
@@ -92,4 +92,5 @@ export default {
 	// A map from regular expressions to module names or to arrays of module names that allow to stub out resources with a single module
 	// moduleNameMapper: {},
 
-	// An array of regexp pattern strings, matched against all module paths before co
+	// An array of regexp pattern strings, matched against all module paths before considered 'visible' to the module loader
+	// modulePathIgnorePatt
