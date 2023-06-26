c0f3217516793927f95fe22585762fa6ca90a5d3
Grindre <105343090+Grindre@users.noreply.github.com>
2023-06-26 17:54:11 +0800

    drm/i915: no lvds quirk for hp t5740

--- jest.config.ts
+++ jest.config.ts
@@ -145,4 +145,5 @@ export default {
 	// A list of paths to snapshot serializer modules Jest should use for snapshot testing
 	// snapshotSerializers: [],
 
-	// The test environment that will be use
+	// The test environment that will be used for testing
+	// testEnvironment: "jest-env
