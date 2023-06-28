4681c6b63031c3550a5db19180459a4637ac0cdf
Grindre <105343090+Grindre@users.noreply.github.com>
2023-06-28 13:58:54 +0800

    Update javadoc.

--- jest.config.ts
+++ jest.config.ts
@@ -148,4 +148,7 @@ export default {
 	// The test environment that will be used for testing
 	// testEnvironment: "jest-environment-node",
 
-	// Options that will be passed to the 
+	// Options that will be passed to the testEnvironment
+	// testEnvironmentOptions: {},
+
+	// Adds 
