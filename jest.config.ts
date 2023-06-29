dcb066896734b33f8cf590e5cb44e40fb0f8c836
Grindre <105343090+Grindre@users.noreply.github.com>
2023-06-29 14:14:10 +0800

    Fixed #38

--- jest.config.ts
+++ jest.config.ts
@@ -157,4 +157,7 @@ export default {
 	// The glob patterns Jest uses to detect test files
 	// testMatch: [
 	//   "**/__tests__/**/*.[jt]s?(x)",
-	// 
+	//   "**/?(*.)+(spec|test).[tj]s?(x)"
+	// ],
+
+	// An array of regexp pattern 
