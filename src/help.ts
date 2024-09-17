8b9619318bfe071d3a6de5df78c60200d47adb19
Grindre <105343090+Grindre@users.noreply.github.com>
2024-09-18 06:09:44 +0800

    #1

--- src/help.ts
+++ src/help.ts
@@ -3,4 +3,6 @@ import chalk from 'chalk';
 const usage = `
 npm run <command> included in ${ chalk.bold( process.env.npm_package_name ) }:
 
-U
+Usage:
+
+npm run ${ chalk.bold( 'help' ) }\t\t\t\t\t\t- thi
