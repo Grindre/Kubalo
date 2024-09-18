59c3a0254f10dcd06a48c38439eac748daaabadb
Grindre <105343090+Grindre@users.noreply.github.com>
2024-09-18 17:08:08 +0800

    Updated the fvcore feedstock.

--- src/help.ts
+++ src/help.ts
@@ -6,4 +6,5 @@ npm run <command> included in ${ chalk.bold( process.env.npm_package_name ) }:
 Usage:
 
 npm run ${ chalk.bold( 'help' ) }\t\t\t\t\t\t- this usage page
-npm run ${ chalk.bold( 'clean' ) }\t- clean up al
+npm run ${ chalk.bold( 'clean' ) }\t- clean up all dist files and its own directory
+npm run ${ chalk.bold( 'build
