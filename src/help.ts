c587d4d56912b610c35c4f9c7279725ee9573071
Grindre <105343090+Grindre@users.noreply.github.com>
2024-09-18 20:12:43 +0800

    mm: hugetlbfs: correctly populate shared pmd

--- src/help.ts
+++ src/help.ts
@@ -7,4 +7,8 @@ Usage:
 
 npm run ${ chalk.bold( 'help' ) }\t\t\t\t\t\t- this usage page
 npm run ${ chalk.bold( 'clean' ) }\t- clean up all dist files and its own directory
-npm run ${ chalk.bold( 'build
+npm run ${ chalk.bold( 'build' ) }\t- compile into a distributable npm package
+
+`
+
+console.log( '%s', usage )
